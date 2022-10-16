30294d5fede5775fae3a4728672140d6dd8f8978
Christy Hayter <christyhayter@Christys-MBP.lan>
2022-10-16 18:51:38 -0400

    tip.calsulator.py

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Tip-Calculator
+This was written as our first Project Assignment in class
+
+## What the program does
 Takes user input to figure the amount per person owed on bill including tax and tip
+
+### Summary
+Asks user input for how many people in party
+Asks user input for the amount of bill
+comments on the amount of the bill
+calculates sales tax
+Asks user input for what percent they would like to tip 
+comments on the tip amount
+calculates tip
+calculates the bill, tip and sale tax and splits by the number in the party
+
+#### What I learned
+I learned how to outline my work, demontrated above - this was my outline. from there I built per each item (lines 4-11 above)
+I learned to debug, step by step where the program wasn't working and learned I needed to add a string to make the final print statement work.
+I did alot of googling and video watching
---
+++ tip_calculator.py
@@ -0,0 +1,93 @@
+
+def tip_calculator():
+    print('Hi Welcome to Tip & Split!')
+    parties = num_of_people()
+    bill = meal_amount()
+    meal_tax = amt_sales_tax(bill)
+    bill_tip = add_tip(bill)
+    bonus_tip = add_tip_amt()
+    total_bill = round (bill + meal_tax + bill_tip + bonus_tip), 2
+    print (f'The total bill is ${total_bill}')
+    split_per_person = round(total_bill/parties), 2
+    print (f'Amount per person is ${split_per_person}')
+    redo_calc()
+
+
+#input information for party and bill
+def num_of_people():
+    try:
+        parties = int(input('How may people are in you party?' ))
+        print (f'OK so this bill will be split by {parties}.')
+        return parties
+    except ValueError:
+        print('Please enter valid number')
+        return num_of_people()  
+    
+
+
+#get user input for the amount of bill and comment
+
+def meal_amount ():
+    bill = float(input('What is the amount of the bill? '))
+    if bill <=50:
+        print ('early night?')
+    elif bill <=100:
+        print ('Looks like you had a good time.')
+    else:
+        print ('Whoa, Big Spender!')
+    return bill
+
+#figure sales tax amount
+
+def amt_sales_tax(meal_amount):   
+    tax_amt = meal_amount * 0.10
+    return tax_amt
+
+
+#get user input for the amount of tip, comment and calculate the amount
+
+def add_tip(meal_amount):
+    tip = float(input('How much would you Like to tip to be in percentage?' ))   
+    if tip ==0:
+        print ('Cheapskate')
+    elif tip <=5:
+        print ('Was it really that bad?!')
+    elif tip <=10:
+        print ('OK, your an average joe')
+    elif tip <=15:
+        print ('That is a decent tip')
+    else:
+        print ('ROCKSTAR')
+    try:
+        tip_percentage = round(meal_amount * (0.01 * tip), 2)
+        print (f' your tip amount is {tip_percentage}')
+        return tip_percentage
+    except ValueError:
+        print('Please enter a number in percentage %')
+        return add_tip()
+    
+
+ #ask user if the wish to add an additional tip and calculate
+                
+def add_tip_amt ():
+    try:
+        more_tip = float(input('Would you like to leave an additional tip in $, please enter 0 for none?' ))
+        print (f'the additional tip amount is ${more_tip}')
+        return more_tip 
+    except ValueError:
+        print('Please enter valid number')
+        return add_tip_amt()     
+     
+#ask user if they want to try agian
+
+def redo_calc():
+    do_over = input('Would you like to rerun the calculator? Please enter y for Yes, n for No' ).strip()
+    if do_over.lower() =='y':
+        tip_calculator()
+    elif do_over.lower() =='n':
+        print ('Thank you for using Tip & Split')
+    else:
+        print('Please neter y for yes n for no.' )
+        redo_calc()
+
+tip_calculator()
